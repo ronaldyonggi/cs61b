9fa55c38fe8286aa659605541721341d46e4cdc9
Ronald Yonggi <ronaldyonggi@Ronalds-MacBook-Air.local>
2019-09-15 18:05:56 -0400

    Updated progress

---
+++ Lecture/Week 1/2. Defining and Using Classes/Dog.java
@@ -0,0 +1,13 @@
+public class Dog{
+    public int weightInPounds;
+    
+    public void makeNoise() {
+        if (weightInPounds < 10) {
+            System.out.println("yip!");
+        } else if (weightInPounds < 30) {
+            System.out.println("bark.");
+        } else {
+            System.out.println("wooooof!");
+        }
+    }
+}
---
+++ Lecture/Week 1/2. Defining and Using Classes/DogLauncher.java
@@ -0,0 +1,7 @@
+public class DogLauncher{
+    /* DogLauncher has a main method! */
+    public static void main(String[] args){
+        /* We want to call the makeNoise method of the Dog class */
+        Dog.makeNoise();
+    }
+}
