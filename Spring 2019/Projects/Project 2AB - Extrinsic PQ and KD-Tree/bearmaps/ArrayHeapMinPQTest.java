17b5b85e65ddc79b622c0cbd8491633ad63235ff
Ronald <ronaldyonggi@gmail.com>
2020-03-12 14:33:22 -0400

    added test2, renamed tests

--- Spring 2019/Projects/Project 2AB - Extrinsic PQ and KD-Tree/bearmaps/ArrayHeapMinPQTest.java
+++ Spring 2019/Projects/Project 2AB - Extrinsic PQ and KD-Tree/bearmaps/ArrayHeapMinPQTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.assertFalse;
 public class ArrayHeapMinPQTest {
 
     @Test
-    public void addRemoveContainsTest(){
+    public void test1(){
         /** Tests add and size.
          *
          */
@@ -27,5 +27,26 @@ public class ArrayHeapMinPQTest {
         String removed2 = x.removeSmallest();
         assertEquals("Lol", removed2);
         assertFalse(x.contains("Lol"));
+    }
+
+    @Test
+    public void test2(){
+        /** Tests getSmallest.
+         *
+         */
+        ArrayHeapMinPQ<String> x = new ArrayHeapMinPQ<>();
+        x.add("Lol", 3);
+        x.add("Huh", 7);
+        x.add("Woah", 1);
+        x.add("Really?", 12);
+        assertEquals("Woah", x.getSmallest());
+        assertEquals(4, x.size());
+
+        /** Tests changePriority.
+         *
+         */
+        x.changePriority("Woah", 9);
+        assertEquals("Lol", x.getSmallest());
+        assertEquals(4, x.size());
     }
 }
