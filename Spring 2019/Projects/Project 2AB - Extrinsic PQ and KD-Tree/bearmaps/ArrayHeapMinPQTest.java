5898b87728ac58368953875b36c43a3c19716a7d
Ronald <ronaldyonggi@gmail.com>
2020-03-12 14:12:47 -0400

    added addRemoveTest

--- Spring 2019/Projects/Project 2AB - Extrinsic PQ and KD-Tree/bearmaps/ArrayHeapMinPQTest.java
+++ Spring 2019/Projects/Project 2AB - Extrinsic PQ and KD-Tree/bearmaps/ArrayHeapMinPQTest.java
@@ -2,6 +2,30 @@ package bearmaps;
 
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 public class ArrayHeapMinPQTest {
+
+    @Test
+    public void addRemoveTest(){
+        /** Tests add and size.
+         *
+         */
+        ArrayHeapMinPQ<String> x = new ArrayHeapMinPQ<>();
+        x.add("Lol", 2);
+        x.add("Huh", 1);
+        assertEquals(2, x.size());
+
+        /** Tests removeSmallest()
+         *
+         */
+        String removed = x.removeSmallest();
+        assertEquals("Huh", removed);
+        assertEquals(1, x.size());
+        assertFalse(x.contains("Huh"));
+
+        String removed2 = x.removeSmallest();
+        assertEquals("Lol", removed2);
+        assertFalse(x.contains("Lol"));
+    }
 }
