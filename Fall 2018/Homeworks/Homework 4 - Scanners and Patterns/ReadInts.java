eb87436fc95144e09be4bad84b5c3aed8baf2119
Ronald <ronaldyonggi@gmail.com>
2020-03-22 11:06:38 -0400

    refactor readInts, implemented smartReadInts

--- Fall 2018/Homeworks/Homework 4 - Scanners and Patterns/ReadInts.java
+++ Fall 2018/Homeworks/Homework 4 - Scanners and Patterns/ReadInts.java
@@ -1,4 +1,5 @@
 import java.lang.reflect.Array;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.util.List;
 import java.util.ArrayList;
@@ -22,7 +23,7 @@ public class ReadInts {
     }
 
     /** Takes a string INPUT consisting of integer literals separated
-     *  by whitespace, and returns all a list of all the ints represented
+     *  by whitespace, and returns a list of all the ints represented
      *  by those numerals in the order they appear in the string. */
     public static List<Integer> readInts(String input) {
         /* The notations ArrayList<Integer> and List<Integer> indicate
@@ -33,6 +34,7 @@ public class ReadInts {
         while (s.hasNext()) {
             int nextInt = s.nextInt();
             //FIXME
+            L.add(nextInt); //ANSWER
         }
         return L;
     }
@@ -48,6 +50,31 @@ public class ReadInts {
         ArrayList<Integer> L = new ArrayList<Integer>();
 
         // FIXME
+        /** Initialize a scanner s.
+         *
+         */
+        Scanner s = new Scanner(input);
+        /** While loop runs as long as there are still tokens available.
+         *
+         */
+        while (s.hasNext()){
+            /** With the try statement, if the next token has a syntax
+             * of numerical, then add it to L.
+             */
+            try {
+                L.add(s.nextInt());
+            }
+
+            /** Otherwise if the next token is not an integer (nextInt automatically
+             * parses the next token into an integer. If the next token is not an integer,
+             * nextInt() will throw an InputMismatchException error), shift to the token after
+             * next token by simply calling next().
+             *
+             */
+            catch (InputMismatchException exp) {
+                s.next();
+            }
+        }
         return L;
     }
 }
